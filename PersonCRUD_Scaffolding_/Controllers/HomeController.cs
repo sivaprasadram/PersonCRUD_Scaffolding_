1338b5775b3823b04a60c0eb97cf9d2f53188393
sivaprasadram <sivaprasadv9323@gmail.com>
2023-04-24 17:55:23 +0530

    List ar in crud  operation R part is complete  I mean Read

--- PersonCRUD_Scaffolding_/Controllers/HomeController.cs
+++ PersonCRUD_Scaffolding_/Controllers/HomeController.cs
@@ -10,21 +10,15 @@ namespace PersonCRUD_Scaffolding_.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            ToolsCS Context = new ToolsCS();
 
-            return View();
-        }
+           var Listofdata = Context.persons.ToList();
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            return View(Listofdata);
 
-            return View();
-        }
+        }         
+
+
     }
 }
